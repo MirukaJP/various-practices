69373bcefd6330535c0a1b12d6562a72a0a38637
Miruka <miruka.jp@gmail.com>
2021-05-18 10:41:43 +0900

    Create Test Component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+    <test-component hoge="It's hoge props." />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import TestComponent from './components/TestComponent.vue';
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    TestComponent,
   },
 };
 </script>
 
 <style lang="scss">
 #app {
+  margin-top: 60px;
+
+  color: #2c3e50;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
+
+  text-align: center;
+
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="TestComponent">
+    Test Component.<br>
+    {{hoge}}
+  </p>
+</template>
+
+<script>
+export default {
+  name: "TestComponent",
+  props: {
+    hoge: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.TestComponent {
+  color: rgb(190, 72, 127);
+}
+</style>
