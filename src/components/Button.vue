83a6722decb669d6d1feabb13453fd039c988b09
Miruka <miruka.jp@gmail.com>
2021-06-13 22:33:14 +0900

    parent component <-> child component

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="c-Button" @click="$emit('test')">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'CButton',
+  props: {
+    text: String,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.c-Button {
+  background-color: $primary;
+  color: #fff;
+}
+</style>
--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,38 +1,61 @@
 <template>
   <div class="TestComponent">
     <p>
-    Test Component.<br>
-    {{ foo }}
+      Test Component.<br>
+      {{ foo }}<br>
+      {{ hoge }}
     </p>
-  <p>
-    {{ skills }}
-  </p>
+    <p>
+      {{ isToggle }}<br>
+      <c-button text="Hey!" @test="ButtonAction" />
+    </p>
+    <transition name="fade">
+      <p :key="isToggle">
+        {{ isToggle ? skills : topics }}
+      </p>
+    </transition>
   </div>
 </template>
 
 <script>
+import CButton from './Button.vue';
 import mySkill from '../datas/mySkill.json';
+import Topics from '../datas/topics.json';
 
 export default {
   name: "TestComponent",
   props: {
     hoge: String,
   },
+  components: {
+    CButton,
+  },
   data () {
     return {
       foo: "It's data foo.",
-      skills: mySkill
+      isToggle: true,
+      skills: mySkill,
+      topics: Topics
     }
   },
-  // methods: {
-  //   MySkill: function() {}
-  // },
-  // created () {}
+  methods: {
+    ButtonAction: function() {
+      this.isToggle = !this.isToggle;
+    }
+  },
+  created () {}
 };
 </script>
 
 <style lang="scss" scoped>
 .TestComponent {
   color: $primary;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
